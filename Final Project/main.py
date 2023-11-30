9f06655998b459111d23fc1abeadb060d72102a2
Aidan Keefe <aidanbkeefe@gmail.com>
2023-11-30 15:10:36 -0500

    added in a year of test data to utilize for demo

--- Final Project/main.py
+++ Final Project/main.py
@@ -5,7 +5,7 @@ import ScreenManager
 
 
 class GameManager:
-    stockHeader = ["Stock", "M1", "M2"] # We'll need to implement a shifting date function
+    stockHeader = ["Stock", "M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10", "M11", "M12"] # We'll need to implement a shifting date function
     data : list = []
     traders : list[Trader]
     currentTime : int
@@ -47,7 +47,7 @@ class GameManager:
     def progress(self):
         # flow of each turn: print stock table -> update stocks for traders -> traderAction -> updateTime
         print("Turn " + str(self.currentTime - self.timeframeStart + 1))
-        if self.currentTime >= len(self.data):
+        if self.currentTime >= len(self.data[0]):
             raise IndexError("Current time has surpassed size of data.")
 
         stockData = self.getData()
@@ -296,14 +296,14 @@ def inputClean(s : str):
 def main():
     playerBal = 100
     testData = [["AMC", 4.5, 7],["GME", 555, 6],["BBBYQ", 8, 999]]
-    demoData = [] # 18 months of values
+    demoData = [["AMC", 4.5, 7, 10, 19, 17, 21, 22, 25, 23.5, 30.1, 28.4, 29], ["SHC", 2.1, 2, 2.4, 3.4, 3, 3.4, 3.2, 4.8, 5, 5.1, 5, 5.1], ["RGL", 32, 30, 26.2, 28.1, 28.4, 27.3, 25.9, 26.7, 24.3, 22, 21.3, 19.5]] # 18 months of values
     player = Trader("Player", playerBal, 0)
     botOne = Trader("Greedy Lad", playerBal, 1)
     botTwo = Trader("Wise Guy", playerBal, 2)
     botThree = Trader("Silly Billy", playerBal, 3)
     traders = [player, botOne, botTwo, botThree]
     gameManager = GameManager() # time frame is set to 1 for now, because of how we read the data
-    gameManager.sData(testData).sTimeframe(1, 3).sTraders(traders)
+    gameManager.sData(demoData).sTimeframe(1, 13).sTraders(traders)
     gameManager.gameStart()
     
 main()
